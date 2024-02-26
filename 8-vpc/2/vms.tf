b85a3abdaaba7f76ef77a4cc5217223e08787c2e
suportemaxtech <148550323+suportemaxtech@users.noreply.github.com>
2024-02-26 10:11:41 -0300

    ajuste

--- 8-vpc/2/vms.tf
+++ 8-vpc/2/vms.tf
@@ -5,6 +5,7 @@ resource "aws_instance" "vm" {
   instance_type = var.instance_type
   key_name      = aws_key_pair.chave_ssh_desafio.key_name
   subnet_id     = aws_subnet.each.value.id 
+  associate_public_ip_address = true
   vpc_security_group_ids = [
     aws_security_group.exercicio_libera_ssh.id
   ]
