3ec96251b9bfb3edfa4feafa103095e2acb9c159
suportemaxtech <148550323+suportemaxtech@users.noreply.github.com>
2024-03-04 17:16:15 -0300

    ajustes

---
+++ 12-ansible/terraform/02/out.tf
@@ -0,0 +1,8 @@
+output "informacoes_vms" {
+  value = [
+    for vm in aws_instance.vm :
+    "ssh -i C:/Cursos/DevOps/terraform/ssh/id_rsa ubuntu@${aws_instance.vm.public_dns} -o ServerAliveInterval=60 "
+  ]
+
+}
+
